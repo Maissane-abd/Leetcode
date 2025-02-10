d4892f75353d7e5886f767d8c6cc1a3aeea5b1e9
Maïssane Abdallah <abd.maissane@gmail.com>
2025-02-10 20:29:36 +0100

    create a Hello World Function

---
+++ index.js
@@ -0,0 +1,17 @@
+// Create Hello World Function esay
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+____________________________________________
+
+// 2620. Counter Easy
+
+
